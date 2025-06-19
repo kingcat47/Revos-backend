77d85a65667a39a4914f5f5d98e3832ec79643ad
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-06-19 16:27:00 +0900

    갑자기 투표 안되던거 수정함.

---
+++ Revos_backend/article/serializers.py
@@ -0,0 +1,3 @@
+from rest_framework import serializers from .models import Article
+
+class ArticleSerializer(serializers.ModelSerializer): class Meta: model = Article fields = ['id', 'title', 'text', 'category', 'img', 'created_at']
